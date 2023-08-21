97e431e0e964838e16d9697eb7716757428f1e7a
elenamayag <142683768+elenamayag@users.noreply.github.com>
2023-08-20 22:48:10 -0500

    Examples added to consolelogexamples02.js

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,9 @@
 console.log("Some Programming Languages:");
 
 console.log("Python\nJavaScript\nJava\nC#\nSwift");
+
+console.log("back-slash n starts a new line")
+
+console.log("My favorite foods are:")
+
+console.log("Steak\nPasta\nPizza")
