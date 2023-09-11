e14cd85794c8a8bf212830e23f39ccfb6db56141
elenamayag <142683768+elenamayag@users.noreply.github.com>
2023-09-10 20:08:33 -0500

    data and variables mod

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,55 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+let shuttleName;
+shuttleName = 'Determination';
+
+let shuttleSpeedMph; 
+shuttleSpeedMph = 17500;
+
+let distanceToMarsKm;
+distanceToMarsKm = 225000000;
+
+let distanceToMoonKm;
+distanceToMoonKm = 38400;
+
+let milesPerKm;
+milesPerKm = 0.621;
+
+
+
+let milesToMars;
+milesToMars = distanceToMarsKm * milesPerKm;
+
+let hoursToMars;
+hoursToMars = milesToMars / shuttleSpeedMph;
+
+let daysToMars; 
+daysToMars = hoursToMars / 24;
+
+
+
+let milesToMoon;
+milesToMoon = distanceToMoonKm * milesPerKm;
+
+let hoursToMoon;
+hoursToMoon = milesToMoon / shuttleSpeedMph
+
+let daysToMoon;
+daysToMoon = hoursToMoon / 24 
+
+
+console.log(shuttleName)
+console.log(shuttleSpeedMph)
+console.log(distanceToMarsKm)
+console.log(distanceToMoonKm)
+console.log(milesPerKm)
+console.log(milesToMars)
+console.log(hoursToMars)
+console.log(daysToMars)
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars. ")
+console.log(milesToMoon)
+console.log(hoursToMoon)
+console.log(daysToMoon)
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon. ")
