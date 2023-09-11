00d19a4452c0a0381024c87434e6e09f9b11fe9b
elenamayag <142683768+elenamayag@users.noreply.github.com>
2023-09-10 20:59:56 -0500

    data and variable mod FINAL

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,15 +1,5 @@
 // Declare and assign the variables below
 
-// Use console.log to print the 'typeof' each variable. Print one item per line.
-
-// Calculate a space mission below
-
-// Print the results of the space mission calculations below
-
-// Calculate a trip to the moon below
-
-// Print the results of the trip to the moon below
-
 let shuttleName;
 shuttleName = 'Determination';
 
@@ -25,8 +15,15 @@ distanceToMoonKm = 38400;
 let milesPerKm;
 milesPerKm = 0.621;
 
+// Use console.log to print the 'typeof' each variable. Print one item per line.
 
+console.log(typeof shuttleName)
+console.log(typeof shuttleSpeedMph)
+console.log(typeof distanceToMarsKm)
+console.log(typeof distanceToMoonKm)
+console.log(typeof milesPerKm)
 
+// Calculate a space mission below
 let milesToMars;
 milesToMars = distanceToMarsKm * milesPerKm;
 
@@ -36,7 +33,13 @@ hoursToMars = milesToMars / shuttleSpeedMph;
 let daysToMars; 
 daysToMars = hoursToMars / 24;
 
+console.log(daysToMars)
 
+// Print the results of the space mission calculations below
+
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars. ")
+
+// Calculate a trip to the moon below
 
 let milesToMoon;
 milesToMoon = distanceToMoonKm * milesPerKm;
@@ -47,17 +50,8 @@ hoursToMoon = milesToMoon / shuttleSpeedMph
 let daysToMoon;
 daysToMoon = hoursToMoon / 24 
 
-
-console.log(shuttleName)
-console.log(shuttleSpeedMph)
-console.log(distanceToMarsKm)
-console.log(distanceToMoonKm)
-console.log(milesPerKm)
-console.log(milesToMars)
-console.log(hoursToMars)
-console.log(daysToMars)
-console.log(shuttleName + " will take " + daysToMars + " days to reach Mars. ")
-console.log(milesToMoon)
-console.log(hoursToMoon)
 console.log(daysToMoon)
+
+// Print the results of the trip to the moon below
+
 console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon. ")
