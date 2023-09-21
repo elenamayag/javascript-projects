4786fa0403dde383487c4d1228e036cbb9ef057e
elenamayag <142683768+elenamayag@users.noreply.github.com>
2023-09-20 20:37:16 -0500

    for-Loop-Exercises.js

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -4,9 +4,28 @@
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
 
+for (let i = 0; i < 21; i++) {
+      console.log(i);
+   }
 
+console.log("--------")
 
+for (let i = 3; i < 30; i=i+2) {
+    console.log(i);
+  }
 
+console.log("--------")
+
+for (let i = 12; i > -15; i=i-2) {
+    console.log(i);
+  }
+
+  console.log("--------")
+
+  for (let i = 50; i > 19; i=i-3) {
+    console.log(i)
+  }
+  console.log("--------")
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
 
@@ -14,11 +33,44 @@ Initialize two variables to hold the string “LaunchCode” and the array [1, 5
 Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
+let str = ("LaunchCode");
+let array =  [1, 5, "LC101", "blue", 42];
 
+ 
 
+for (let i=0; i < array.length; i++) {
+  console.log(array[i])
+}
 
+console.log("--------")
+
+let reversedArr = str.split("").reverse().join("") ;
+let newStr = reversedArr
+for (let i=0; i < str.length ; i++) {
+ console.log(newStr[i])
+}
+console.log("--------")
 
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+  let exArray = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+exArray.sort();
+console.log(exArray)
+
+  let evens = [ ];
+  let odds = [ ];
+
+
+for (let i = 0; i < exArray.length; i++) {
+    
+  if (exArray[i] %2) {
+    evens.push(exArray[i]);
+  } else {
+    odds.push(exArray[i]);
+  }
+} 
+console.log(evens);
+console.log(odds);
+
