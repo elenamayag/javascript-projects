b6ec07bdb65745a7a605dd7493928a27076840ac
elenamayag <142683768+elenamayag@users.noreply.github.com>
2023-10-19 19:34:03 -0500

    Class exercise

--- classes/exercises/ClassExercises.js
+++ classes/exercises/ClassExercises.js
@@ -1,10 +1,57 @@
 // Define your Book class here:
-
+class Book {
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+       this.title = title;
+       this.author = author;
+       this.copyright = copyright;
+       this.isbn = isbn;
+       this.pages = pages;
+       this.timesCheckedOut = timesCheckedOut;
+       this.discarded = discarded;
+    }
+ 
+    checkout(uses=1) {
+       this.timesCheckedOut += uses;
+    }
+ }
+ 
+ class Manual extends Book {
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+       super(title, author, copyright, isbn, pages, timesCheckedOut, discarded);
+    }
+ 
+    dispose(currentYear){
+       if (currentYear-this.copyright > 5) {
+          this.discarded = 'Yes';
+       }
+    }
+ }
+ 
+ class Novel extends Book {
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+       super(title, author, copyright, isbn, pages, timesCheckedOut, discarded);
+    }
+ 
+    dispose(){
+       if (this.timesCheckedOut > 100) {
+          this.discarded = 'Yes';
+       }
+    }
+ }
 
 // Define your Manual and Novel classes here:
+let makingTheShip = new Manual('Top Secret Shuttle Building Manual', 'Redacted', 2013, '0000000000000', 1147, 1, 'No');
 
+let goodReading = new Novel('Pride and Prejudice', 'Jane Austen', 1813, '1111111111111', 432, 32, 'No' )
 
 // Declare the objects for exercises 2 and 3 here:
+let books = [makingTheShip, goodReading];
+for (let i = 0; i < this.books.length; i++) {
+    console.log(`Are you sure you'd like to discard${books[i].name}? \n ${books[i].dispose()} has now been discarded.`);
+}
 
 
 // Code exercises 4 & 5 here:
+
+goodReading.checkout(5);
+goodReading.dispose();
